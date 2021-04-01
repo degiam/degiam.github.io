0fb9c1f5bdf131f83cf08fc1e0d5573a69eaa697
degiam <59824909+degiam@users.noreply.github.com>
2021-04-01 10:34:04 +0700

    Add files via upload

--- pro/soklin/assets/js/main.js
+++ pro/soklin/assets/js/main.js
@@ -19,11 +19,17 @@
 
 // Account Name
 	function name() {
-		let head = document.head
+		let name = 'style'
+
+		let old = $('#' + name)
+		if ( old !== null ) old.remove()
+
+		let slot = document.querySelector('slot')
 			width = document.querySelector('header nav:last-child > * span').clientWidth
 			css = document.createElement('style')
+			css.id = name
 			css.innerHTML = 'header nav:last-child > * span { width: 0 } header nav:last-child > *:hover span { width: ' + (width + 15) + 'px }'
-		head.appendChild(css)
+		slot.appendChild(css)
 	}
 
 	name()
